283903c40e2a37fefa6fa9f97bdec1f1e4c7dab1
Andras Huszar <huszarandris@gmail.com>
2024-02-06 12:25:00 +0100

    fix stock dto

--- src/dtos/stock.dto.ts
+++ src/dtos/stock.dto.ts
@@ -1,11 +1,7 @@
-import { IsDecimal, IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateStockDto {
   @IsString()
   @IsNotEmpty()
   public symbol: string;
-
-  @IsDecimal()
-  @IsNotEmpty()
-  public price: number;
 }
